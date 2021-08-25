a289496d534ad4593f80ab51e8c55ef8e0c38ab6
Bruno Mendes <mendessbruno@gmail.com>
2021-08-25 13:52:18 -0300

    Updates

---
+++ precache-manifest.bd66c6fbef29df3ca0bcd2f146036104.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f15a72221e6cb4b1c8d4d95d45546d0",
+    "url": "/sd-010-a-project-frontend-online-store.io/index.html"
+  },
+  {
+    "revision": "ea9c62b7736958eb3222",
+    "url": "/sd-010-a-project-frontend-online-store.io/static/css/main.7422706c.chunk.css"
+  },
+  {
+    "revision": "fd03f8205a4cc11aa2e9",
+    "url": "/sd-010-a-project-frontend-online-store.io/static/js/2.06b5e5be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/sd-010-a-project-frontend-online-store.io/static/js/2.06b5e5be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9c62b7736958eb3222",
+    "url": "/sd-010-a-project-frontend-online-store.io/static/js/main.970f776a.chunk.js"
+  },
+  {
+    "revision": "fa1da2c03202781ef799",
+    "url": "/sd-010-a-project-frontend-online-store.io/static/js/runtime-main.38f275ff.js"
+  },
+  {
+    "revision": "5cb1cb3edca4c8962062796edad54987",
+    "url": "/sd-010-a-project-frontend-online-store.io/static/media/mercadolivre.5cb1cb3e.png"
+  }
+]);
